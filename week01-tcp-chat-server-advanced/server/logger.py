d0bf286e4147ebd07d8ba65ada4f26fa91a08a1e
Catsack423 <face11773@gmail.com>
2026-01-31 17:23:05 +0700

    renam folder

--- week01-tcp-chat-server-advanced/server/logger.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-def log_message(message):
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    formatted_msg = f"[{timestamp}] {message}"
-    print(formatted_msg)
-    with open("server_log.txt", "a", encoding="utf-8") as f:
-        f.write(formatted_msg + "\n")
