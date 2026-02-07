862e9048a4c5c1ea9e0e9a0733e102b61b747f1a
Catsack423 <face11773@gmail.com>
2026-02-07 23:47:52 +0700

    do week03

---
+++ week03-udp-broadcast-basic/listener.py
@@ -0,0 +1,12 @@
+# listener.py
+import socket
+from config import PORT, BUFFER_SIZE
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("", PORT))  # Listen on all interfaces
+
+print(f"[LISTENER] Listening for broadcast on port {PORT}")
+
+while True:
+    data, addr = sock.recvfrom(BUFFER_SIZE)
+    print(f"[LISTENER] From {addr}: {data.decode()}")
